2e22b34720b9336aeac8a2dca217555c557a0090
Garrett Russell <garrettrussell@Garretts-MacBook-Pro.local>
2022-11-19 20:05:14 -0800

    changed urls, added volume algo

--- src/App.js
+++ src/App.js
@@ -1,90 +1,135 @@
 import { useState, useEffect } from "react";
 import React from "react";
-import Intraday from "./algorithms.js";
+import { getDates, Volume } from "./algorithms.js";
 
 function App() {
-  const ticker_list = ["AAPL", "NVDA", "IBM"]; //testing placeholders
+  const ticker_list = [
+    "AAPL",
+    "IBM",
+    "AMZN",
+    "GOOG",
+    "TSLA",
+    "PEP",
+    "COST",
+    "META",
+    "AVGO",
+    "CSCO",
+    "NFLX",
+    "INTC",
+    "PYPL",
+    "NVDA",
+    "UNH",
+  ];
   const time_frame = [
-    "1 Minute",
     "5 Minute",
     "15 Minute",
     "30 Minute",
     "60 Minute",
-  ]; //testing placeholders
+    "Daily",
+    "Weekly",
+  ];
   const API_KEY = "0X7FGLTBB3E4SVWG";
   const API_REQUEST_BASE = "https://www.alphavantage.co/query?function="; //base of the API URL
 
   const [ticker_select, setTicker] = useState(ticker_list[0]);
   const [time_frame_select, setTimeFrame] = useState(time_frame[0]);
-  const [data, setData] = useState({});
-  const [trigger, setTrigger] = useState(false);
-  let parsed = [];
+  let dates = [];
+  let url = ``;
+  let time_slice = "";
 
-  // const logChange = useEffect(() => {
-  //   console.log('new value of ans ', ans);
-  // }, [ans]);
-
-  const logChange = useEffect(() => {
-    let url = `${API_REQUEST_BASE}TIME_SERIES_INTRADAY&symbol=${ticker_select}&interval=1min&outputsize=full&apikey=0X7FGLTBB3E4SVWG`;
-    const data = fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      });
-  }, [trigger]); // pass `value` as a dependency
-
-  const submit = (e) => {
+  const submit = async (e) => {
     e.preventDefault();
-    setTrigger(!trigger); //fetch data from API
 
-    /*TO DO 
-    Conditional logic for which algorithm function to call based on time_frame_select
-    */
+    if (time_frame_select == "5 Minute") {
+      time_slice = "5min";
+    } else if (time_frame_select == "15 Minute") {
+      time_slice = "15min";
+    } else if (time_frame_select == "30 Minute") {
+      time_slice = "30min";
+    } else if (time_frame_select == "60 Minute") {
+      time_slice = "60min";
+    } else if (time_frame_select == "Daily") {
+      time_slice = "Daily";
+    } else {
+      time_slice = "Weekly";
+    }
 
-    parsed = Intraday(time_frame_select, data);
+    if (time_slice == "Daily") {
+      url = `${API_REQUEST_BASE}TIME_SERIES_DAILY_ADJUSTED&symbol=${ticker_select}&apikey=0X7FGLTBB3E4SVWG`;
+    } else if (time_slice == "Weekly") {
+      url = `${API_REQUEST_BASE}TIME_SERIES_WEEKLY_ADJUSTED&symbol=${ticker_select}&apikey=0X7FGLTBB3E4SVWG`;
+    } else {
+      url = `${API_REQUEST_BASE}TIME_SERIES_INTRADAY&symbol=${ticker_select}&interval=${time_slice}&outputsize=full&apikey=0X7FGLTBB3E4SVWG`;
+    }
+
+    //fetching data
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+
+    if (time_slice == "Daily" || time_slice == "Weekly") {
+      dates = getDates(time_slice, data);
+    } else {
+      dates = getDates(time_slice, data);
+      console.log(dates);
+      //performance(time_slice, data);
+    }
+
+    let volume = Volume(time_slice, data);
+    console.log(volume[0].get("date"), volume[0].get("volume"));
+    console.log(volume[1].get("date"), volume[1].get("volume"));
+    console.log(volume[2]);
   };
 
   return (
-    <form>
-      <select value={ticker_select} onChange={(e) => setTicker(e.target.value)}>
-        {ticker_list.map((value) => (
-          <option value={value} key={value}>
-            {value}
-          </option>
-        ))}
-      </select>
-      <select
-        value={time_frame_select}
-        onChange={(e) => setTimeFrame(e.target.value)}
-      >
-        {time_frame.map((value) => (
-          <option value={value} key={value}>
-            {value}
-          </option>
-        ))}
-      </select>
-      <button type="button" onClick={submit}>
-        Submit
-      </button>
-      <p>{ticker_select}</p>
-      <p>{time_frame_select}</p>
-    </form>
+    <div>
+      <form>
+        <select
+          value={ticker_select}
+          onChange={(e) => setTicker(e.target.value)}
+        >
+          {ticker_list.map((value) => (
+            <option value={value} key={value}>
+              {value}
+            </option>
+          ))}
+        </select>
+        <select
+          value={time_frame_select}
+          onChange={(e) => setTimeFrame(e.target.value)}
+        >
+          {time_frame.map((value) => (
+            <option value={value} key={value}>
+              {value}
+            </option>
+          ))}
+        </select>
+        <button type="button" onClick={submit}>
+          Submit
+        </button>
+        <p>{ticker_select}</p>
+        <p>{time_frame_select}</p>
+      </form>
+    </div>
   );
 }
 
 export default App;
 
-//this is an example of how to access 1 min intraday open values "Time Series (1min)"][key]["1. open"])
-//DONT RUN IT, ITS CASE SENSITIVE BOYS
+/*
 
-//Data return example: 1. open 2. high 3. low 4. close 5. volume
+object 1:
+table
+time_slice      performance (of time slice)      volume 
 
-/*- TODO 
-  - 15min+ and daily, 
-  -display as tabs
-  -code splitting for readability.
-  -misc. data (best/worst performing increment of time), maybe average performer over weekdays etc etc
+object 2:
+chart of object 1
+
+object 3:
+box
+highest volume
+lowest volume
+best performing time slice
+worst performing time slice
+
 */
-
-// KNOWN BUGS: useEffect triggers twice upon first render
--- src/algorithms.js
+++ src/algorithms.js
@@ -1,17 +1,66 @@
-export default function Intraday(time_frame_select, data) {
+export function getDates(time_slice, data) {
   let parsed = [];
-  console.log("timeframe_select: " + time_frame_select);
-  console.log(data);
-  console.log("debugging probe");
-  if (time_frame_select == "1 Minute") {
-    console.log("did enter");
-    for (var key in data["Time Series (1min)"]) {
-      parsed.push(data["Time Series (1min)"][key]["1. open"]);
+  if (time_slice == "Weekly") {
+    for (var key in data["Weekly Adjusted Time Series"]) {
+      parsed.unshift(key);
     }
   }
-  console.log(parsed);
+  for (var key in data[`Time Series (${time_slice})`]) {
+    parsed.unshift(key);
+  }
   return parsed;
 }
 
-//this["Time Series (1min)"]["2022-10-31 04:01:00"]
-//CASE SENSITIVE BOYS
+//highest/lowest index
+export function Volume(time_slice, data) {
+  if (time_slice == "Daily" || time_slice == "Weekly") {
+    return; //no volume returned in API
+  } else {
+    var parsed = [];
+    //load/convert parsed values -> int array
+    for (var key in data[`Time Series (${time_slice})`]) {
+      parsed.unshift(data[`Time Series (${time_slice})`][key]["5. volume"]);
+    }
+    parsed = parsed.map(Number);
+
+    //find highest volume
+    let highestVol = Math.max(...parsed);
+    let lowestVol = Math.min(...parsed);
+
+    //find lowest volume
+    var highIndex = parsed.indexOf(Math.max(...parsed));
+    var lowIndex = parsed.indexOf(Math.min(...parsed));
+
+    //find dates of high/low volume
+    let dates = getDates(time_slice, data);
+    let highDate = dates[highIndex];
+    let lowDate = dates[lowIndex];
+
+    let highReturnKey = new Map();
+    highReturnKey.set("date", highDate);
+    highReturnKey.set("volume", highestVol);
+
+    let lowReturnKey = new Map();
+    lowReturnKey.set("date", lowDate);
+    lowReturnKey.set("volume", lowestVol);
+
+    return [highReturnKey, lowReturnKey, parsed];
+  }
+}
+
+// export function performance(time_slice, data) {
+//   let parsed = [];
+//   let performances = [];
+//   var bestPerformace = 0;
+//   var worstPerformance = 0;
+
+//   for (var key in data[`Time Series (${time_slice})`]) {
+//     parsed.unshift(data[`Time Series (${time_slice})`][key]["4. close"]);
+//   }
+//   parsed = parsed.map(Number);
+
+//   for (var i = 0; i < parsed.length(); i++) {
+//     parsed[i] = 0;
+//   }
+//   console.log(parsed);
+// }
